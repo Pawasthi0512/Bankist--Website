c8a2e1dd6ad3c0278adcbef24746532ce6e16825
Pawasthi0512 <pawasthi0512@gmail.com>
2021-08-26 00:26:49 +0530

    Creation, Addition, Insertion of HTML Elements

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,7 +18,6 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
@@ -29,3 +29,40 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+/////////////////////////////////////////////
+//..........selection of elements............
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header=document.querySelector('.header');
+const allSelect=document.querySelectorAll('.section');//it will give a node list of all element.
+
+// console.log(document.getElementById('section--1'))
+const allButton=document.getElementsByTagName('button')//it will return html live selection.
+// console.log(document.getElementsByClassName('btn'))//it will return html live selection.
+// console.log(header);
+// console.log(allButton);
+
+//........creating HTML Elements using javaScript........
+const message=document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent='We are using cookies to recommend songs <button class="btn btn--close-cookie">"Got It!"</button>'
+message.innerHTML='We are using cookies to recommend songs <button class="btn btn--close-cookie">"Got It!"</button>'
+console.log(message);
+
+//........inserting HTML Elements............
+// header.prepend(message);//it will insert this element as first child of header
+header.append(message);// it will insert this element as last child of header.
+// header.append(message.cloneNode(true))//it will clone this message class so that message can be shown multiple places.
+
+// header.before(message);//It will insert 'message' class as sibling before header.
+// header.after(message);//It will insert 'message' class as sibling after header.
+
+document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+  message.remove();
+})
