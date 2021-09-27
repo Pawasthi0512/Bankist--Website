9e8dfd1db3a3a18555fadc142642f86e95ae1dbb
Pawasthi0512 <pawasthi0512@gmail.com>
2021-09-28 00:10:01 +0530

    Implementing lazy section revealing

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo=document.querySelector('.btn--scroll-to');
 const section1=document.querySelector('#section--1');
+const nav=document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -203,7 +204,6 @@ const handelHover = function(e,opacity){
     logo.style.opacity=opacity;
   }
 }
-const nav=document.querySelector('.nav');
 nav.addEventListener('mouseover',function(e){
   handelHover(e,0.5);
 })
@@ -211,4 +211,40 @@ nav.addEventListener('mouseout',function(e){
   handelHover(e,1);
 })
 
+//.....................................................................................................
+
+
+
+//...................Adding sticky  navigation to web page.............................................
+window.addEventListener('scroll',function(e){
+  const s1Coords=section1.getBoundingClientRect();
+  // console.log(window.scrollY);
+  // console.log(s1Coords);
+  if(window.scrollY>s1Coords.top){
+    nav.classList.add('sticky');
+  }
+  else{
+    nav.classList.remove('sticky');
+  }
+});
+//.....................................................................................................
+
+
+//.................lazy section revealing..............................................................
+const allSections=document.querySelectorAll('.section');
+const revealSection=function(entries,observer){
+  const [entry]=entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+const sectionObserver=new IntersectionObserver(revealSection,{
+  root:null,
+  threshold:.15,
+});
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 //.....................................................................................................
